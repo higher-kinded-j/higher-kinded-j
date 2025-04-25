9f62e723d6f8c82d844d2768ec9d19353d0412bc
MagnusSmith <magnus.smith@gmail.com>
2025-04-25 10:02:23 +0100

    added reader and writer monads

---
+++ src/main/java/org/simulation/hkt/reader/ReaderMonad.java
@@ -0,0 +1,27 @@
+package org.simulation.hkt.reader;
+
+import org.jspecify.annotations.NonNull;
+import org.simulation.hkt.Kind;
+import org.simulation.hkt.Monad;
+import java.util.function.Function;
+import static org.simulation.hkt.reader.ReaderKindHelper.*;
+
+public class ReaderMonad<R> extends ReaderApplicative<R> implements Monad<ReaderKind<R, ?>> {
+
+  @Override
+  public <A, B> @NonNull Kind<ReaderKind<R, ?>, B> flatMap(
+      @NonNull Function<A, Kind<ReaderKind<R, ?>, B>> f,
+      @NonNull Kind<ReaderKind<R, ?>, A> ma) {
+
+    Reader<R, A> readerA = unwrap(ma);
+
+    // flatMap implementation: r -> f(readerA(r)).run(r)
+    // The function 'f' returns a Kind, which needs to be unwrapped inside.
+    Reader<R, B> readerB = readerA.flatMap(a -> {
+      Kind<ReaderKind<R, ?>, B> kindB = f.apply(a);
+      return unwrap(kindB); // Adapt the function f to return Reader<R, B>
+    });
+
+    return wrap(readerB);
+  }
+}
---
+++ src/main/java/org/simulation/hkt/writer/WriterFunctor.java
@@ -0,0 +1,24 @@
+package org.simulation.hkt.writer;
+
+import org.jspecify.annotations.NonNull;
+import org.simulation.hkt.Functor;
+import org.simulation.hkt.Kind;
+
+import java.util.function.Function;
+import static org.simulation.hkt.writer.WriterKindHelper.*;
+
+public class WriterFunctor<W> implements Functor<WriterKind<W, ?>> {
+  // Note: Functor instance doesn't need the Monoid, but Monad/Applicative will.
+
+  @Override
+  public <A, B> @NonNull Kind<WriterKind<W, ?>, B> map(
+      @NonNull Function<A, B> f,
+      @NonNull Kind<WriterKind<W, ?>, A> fa) {
+    // 1. Unwrap to get the underlying Writer<W, A>
+    Writer<W, A> writerA = unwrap(fa);
+    // 2. Use the Writer's own map method
+    Writer<W, B> writerB = writerA.map(f);
+    // 3. Wrap the result back into the Kind
+    return wrap(writerB);
+  }
+}
