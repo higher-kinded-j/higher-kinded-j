c3e1a5057c2e737e9b83dfeb38e3d9971c41477f
Magnus Smith <magnus.smith@gmail.com>
2025-10-28 18:39:26 +0000

    :Selective and choice

--- hkj-api/src/main/java/module-info.java
+++ hkj-api/src/main/java/module-info.java
@@ -2,8 +2,9 @@
 module org.higherkindedj.api {
   requires static org.jspecify;
   requires transitive org.higherkindedj.annotations;
+    requires org.higherkindedj.api;
 
-  exports org.higherkindedj.hkt;
+    exports org.higherkindedj.hkt;
   exports org.higherkindedj.hkt.function;
   exports org.higherkindedj.optics;
 }
